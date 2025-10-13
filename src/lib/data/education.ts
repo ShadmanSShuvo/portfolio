1285bb37fc022f3e92e68d2a6059b5d5842d120d
Md. Shadman Shahriyar Shuvo <shuvobuet23@gmail.com>
2025-10-13 16:10:07 +0600

    first commit

---
+++ src/lib/data/education.ts
@@ -0,0 +1,34 @@
+export const education_milestone = [
+    {
+        institution: "Bangladesh University of Engineering & Technology (BUET)",
+        timeline: "August 2024 - Present",
+        degree: "B.Sc. in Computer Science and Engineering",
+        grade: "CGPA: 3.88/4",
+        description:
+            "Currently enrolled in the CSE program at BUET, one of the most prestigious engineering universities in Bangladesh. I am actively involved in various academic and extracurricular activities.",
+        courses: [
+            "CSE 101 - Structured Programming Language",
+            "CSE 103 - Discrete Mathematics",
+            "CSE 105 - Data Structures and Algorithms I",
+            "CSE 107 - Object Oriented Programming",
+            "MATH 141 - Calculus I",
+            "MATH 143 - Linear Algebra",
+        ],
+    },
+    {
+        institution: "Notre Dame College, Dhaka",
+        timeline: "2022-2023",
+        degree: "Higher Secondary Certificate (HSC)",
+        grade: "GPA: 5.00/5.00",
+        description: "Ranked among the top 2% of students.",
+        courses:null, //["Physics", "Chemistry", "Math", "Biology", "ICT"]//
+    },
+    {
+        institution: "Rangpur Zilla School, Rangpur",
+        timeline: "2013-2021",
+        degree: "Secondary School Certificate (SSC)",
+        grade: "GPA: 5.00/5.00",
+        description: "Achieved the highest academic rank in my batch.",
+        courses: null,
+    },
+];
