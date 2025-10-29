aa07498d67c29b76353fe4c9c13740350125705d
Md. Shadman Shahriyar Shuvo <shuvobuet23@gmail.com>
2025-10-30 01:13:28 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  base: '/portfolio/', // ensures built asset urls are prefixed correctly
+  plugins: [sveltekit()]
 });
